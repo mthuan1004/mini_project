4d2e67136ed004caf4d17509b120323d3e4936c1
Thuan <mthuan1004@gmail.com>
2024-09-13 03:40:40 +0700

    .

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,7 @@
 import ProductCard from './ProductCard';
 
 const products = [
-  { id: 1, name: 'Gạo Jasmine', price: '50.000', description: 'Gạo tẻ chất lượng cao', img: 'https://tvfood.com.vn/vnt_upload/product/09_2023/Jasmine_2.png' },
+  { id: 1, name: 'Gạo Jasmine', price: '50.000', description: 'Gạo Jasmine có hương thơm nhẹ và hạt gạo dài, phù hợp cho các món ăn cao cấp.', img: 'https://tvfood.com.vn/vnt_upload/product/09_2023/Jasmine_2.png' },
   { id: 2, name: 'Gạo Lứt', price: '60.000', description: 'Gạo lứt dinh dưỡng', img: 'https://sieuthigao.vn/wp-content/uploads/2013/08/gao-lut-do-co-may-tui-2-5-kg-hut-chan-khong.png' },
   { id: 3, name: 'Gạo Nàng Hoa', price: '55.000', description: 'Hạt gạo dài, màu đồng nhất, khi nấu có hương thơm tự nhiên.', img: 'https://vuagaovn.com/upload/1/products/l_1570623492_go_nang_hoa_1.png'},
   { id: 4, name: 'Gạo Thơm Lài', price: '40.000', description: 'Hạt gạo thon dài, trắng đồng đều, cơm chín hạt mềm, dẻo vừa và thơm nhẹ, khi nhai có vị ngọt hậu.', img: 'https://vuagaovn.com/upload/1/products/l_128401299_go_thm_lai_2.png'},
